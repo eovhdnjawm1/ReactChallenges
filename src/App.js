3c1f860625628284aa4ac10defeb2d1df7103a90
YoungJinGo <skyyj32@naver.com>
2022-06-06 04:20:09 +0900

    styled components props Setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import styled from "styled-components"
+
+const Father = styled.div`
+  display:flex;
+`
+const Box = styled.div`
+background-color: ${(props) => props.bgColor};
+width: 100px;
+height: 100px;
+`
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Father>
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+    </Father>
   );
 }
 
