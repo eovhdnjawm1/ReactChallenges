d179c262d25c9a3095c0b71a3fd2cba6e26f1892
YoungJinGo <skyyj32@naver.com>
2022-06-11 04:30:35 +0900

    Feat Chart Update

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,69 @@
-function Chart() {
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+import ApexChart from "react-apexcharts"
+
+interface IHistorical {
+	time_open: string;
+	time_close: string;
+	open: number;
+	high: number;
+	low: number;
+	close: number;
+	volume: number;
+	market_cap: number;
+}
+
+interface Chartprops {
+	coinId: string;
+}
+function Chart({ coinId }: Chartprops) {
+	const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
 	return (
-		<h1>Chart</h1>
+		<>
+			{isLoading ? "Loading chart..." : <ApexChart
+				type="line"
+				series={[
+					{
+						name: "closePrice",
+						data: data?.map(((price) => price.close)) ?? [],
+					},
+				]}
+				options={{
+					theme: {
+						mode: "dark",
+					},
+					chart: {
+						height: 300,
+						width: 500,
+						toolbar: {
+							show: false
+						},
+						background: "transparents",
+					},
+					grid: {
+						show: false,
+					},
+					stroke: {
+						curve: "smooth",
+						width: 5,
+					},
+					xaxis: {
+						labels: {
+							show: false,
+						},
+						axisTicks: {
+							show: false,
+						},
+						axisBorder: {
+							show: false,
+						},
+
+					},
+					yaxis: {
+						show: false,
+					},
+				}} />}
+		</>
 	)
 }
 
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -217,7 +217,7 @@ function Coin() {
 							<Price />
 						</Route>
 						<Route path={`/:coinId/chart`}>
-							<Chart />
+							<Chart coinId={coinId} />
 						</Route>
 					</Switch>
 				</>
