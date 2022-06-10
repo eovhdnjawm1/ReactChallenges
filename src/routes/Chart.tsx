08ccf6dcd11667619b084a31a47fea41f7796de0
YoungJinGo <skyyj32@naver.com>
2022-06-11 04:55:45 +0900

    Feat Helmet Update

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query';
 import { fetchCoinHistory } from '../api';
 import ApexChart from "react-apexcharts"
 
+
 interface IHistorical {
 	time_open: string;
 	time_close: string;
@@ -17,7 +18,10 @@ interface Chartprops {
 	coinId: string;
 }
 function Chart({ coinId }: Chartprops) {
-	const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+	const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId),
+		{
+			refetchInterval: 10000,
+		});
 	return (
 		<>
 			{isLoading ? "Loading chart..." : <ApexChart
@@ -50,6 +54,7 @@ function Chart({ coinId }: Chartprops) {
 					xaxis: {
 						labels: {
 							show: false,
+							datetimeFormatter: { month: "mmm 'yy" }
 						},
 						axisTicks: {
 							show: false,
@@ -57,11 +62,24 @@ function Chart({ coinId }: Chartprops) {
 						axisBorder: {
 							show: false,
 						},
+						type: "datetime",
+						categories: data?.map((date) => date.time_close),
 
 					},
 					yaxis: {
 						show: false,
 					},
+					fill: {
+						type: "gradient",
+						gradient: { gradientToColors: ["blue"], stops: [0, 100] },
+
+					},
+					colors: ["red"],
+					tooltip: {
+						y: {
+							formatter: (value) => `$ ${value.toFixed(2)}`
+						},
+					}
 				}} />}
 		</>
 	)
