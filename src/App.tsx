c1700300cd2e0f230302cc7fd50c0b9b43b21e68
YoungJinGo <skyyj32@naver.com>
2022-06-29 23:45:56 +0900

    Get Selectors App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useRecoilState } from 'recoil';
-import { minuteState } from './atoms';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { minuteState, hourSelector } from './atoms';
+
 
 
 function App() {
-  const [minutes, setminutes] = useRecoilState(minuteState)
+  // const [minutes, setminutes] = useRecoilState(minuteState)
   // useRecoilState = 읽고 쓰려고할 때 사용 
   // 상태가 업데이트 되면 자동으로 리렌더링
 
   // useRecoilValue() recoil 상태 값을 반환 / 읽을 수만 있음
   // useSetRecoilState - 읽지않고 업데이트만 할때 사용한다
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
 
   return (
-    <>
-      <input type="number" placeholder='Minutes'></input>
-      <input type="number" placeholder='Minutes'></input>
-    </>
+
+
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
   );
 }
 
